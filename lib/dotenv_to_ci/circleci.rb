adca009393439a7c223aff07ef96e9bc3bbdd880
Rob Kaufman <rob@notch8.com>
2020-06-23 10:55:59 -0700

    Add YAML

--- lib/dotenv_to_ci/circleci.rb
+++ lib/dotenv_to_ci/circleci.rb
@@ -1,6 +1,7 @@
 require 'dotenv'
 require 'json'
 require 'rest-client'
+require 'yaml'
 
 module DotenvToCI
   class CircleCI
@@ -28,6 +29,10 @@ module DotenvToCI
     def load_from_env
       if @provider == "circleci"
         @token ||= ENV["CIRCLECI_TOKEN"]
+        if !@token
+          file = File.expand_path('~/.circleci/cli.yml')
+          @token = YAML.load_file(file)['token'] if File.exists?(file)
+        end
       end
     end
 
